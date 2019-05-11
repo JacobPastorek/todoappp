4ededf29d28f4a316790fba54137bcb16a2a673d
Jakub Pastorek <jacob.pastorek@wazzupa.com>
2019-05-11 16:30:54 +0200

    Updates

---
+++ precache-manifest.4225d69be7434c9becaf17c2c552ff0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0b5098e59be0f14405245688239198",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "56bb119d4284b1620824",
+    "url": "/todoapp/static/css/main.4ad0b95c.chunk.css"
+  },
+  {
+    "revision": "72ea73834752257564ed",
+    "url": "/todoapp/static/js/2.afe515da.chunk.js"
+  },
+  {
+    "revision": "56bb119d4284b1620824",
+    "url": "/todoapp/static/js/main.d01994d8.chunk.js"
+  },
+  {
+    "revision": "3898a5fd3fd40dce9445",
+    "url": "/todoapp/static/js/runtime~main.00cdf33a.js"
+  }
+]);
